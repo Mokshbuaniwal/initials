93dfab7e679444f4baafc96f9f3faaad68c035f7
moksh <moksh@mokshs-MacBook-Air.local>
2025-07-22 17:26:59 +0530

    add

--- project_4.py
+++ project_4.py
@@ -5,7 +5,7 @@
 # here calc start
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-c = input("Enter operation (add , sub , multi , div , div with nearest integer , exponation , reminder ): ")
+c = input("Enter operation (add , sub , multi , div , div with nearest integer , exponation , reminder ,  percentage , factorial): ")
 
 if c == "add":
     print(f"Result: {a + b}")
@@ -30,6 +30,17 @@ elif c == "reminder":
         print(f"Result: {a % b}")
     else:
         print("Error: Division by zero is not allowed.")
+elif c == "percentage":
+    if b != 0:
+        print(f"Result: {(a / b) * 100}%")
+    else:
+        print("Error: Division by zero is not allowed.")
+elif c == "factorial":
+    import math
+    if a >= 0:
+        print(f"Result: {math.factorial(a)}")
+    else:
+        print("Error: Factorial is not defined for negative numbers.")
 else:
     print("Invalid operation! Please choose from add, sub, multi, div, div with nearest integer, exponation, or reminder.")
 
